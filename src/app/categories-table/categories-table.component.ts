d3decceb73f17d35ced7130cbfd55b6d64c279e5
Rivky Shoham <ritsho@gmail.com>
2023-12-28 21:54:14 +0200

    added 3 components

---
+++ src/app/categories-table/categories-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories-table.component.html',
+  styleUrl: './categories-table.component.css'
+})
+export class CategoriesTableComponent {
+
+}
