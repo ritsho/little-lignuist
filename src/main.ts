ae449e3e827d920308404e55d14577631cee33d1
Rivky Shoham <ritsho@gmail.com>
2024-01-03 00:12:30 +0200

    WordsCategory to interface

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,37 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-export enum Language {
+export enum LanguageEnum {
   Hebrew,
   English,
 }
 
-export class WordsCategory {
+export interface WordsCategory {
   name: string;
   id: number;
   last_change_date: Date;
-  target_lang: Language;
-  origin_lang: Language;
+  target_lang: LanguageEnum;
+  origin_lang: LanguageEnum;
   zugot: Map<string,string>;
-
-  constructor(name: string, id: number, last_change_date: Date, 
-              origin_lang: Language, target_lang: Language, 
-              zugot: Map<string,string>) {
-    this.name = name;
-    this.id = id;
-    this.last_change_date = last_change_date;
-    this.origin_lang = origin_lang;
-    this.target_lang = target_lang;
-    this.zugot = zugot;
-  }
 }
+
+export const LIST_OF_CATEGORIES = [
+  {
+    name: "Food", id: 1, last_change_date: new Date("1/1/2024"),
+    origin_lang: LanguageEnum.English, target_lang: LanguageEnum.Hebrew,
+    zugot: new Map<string, string>([["Apple", "תפוח"], ["Orange", "תפוז"], ["Banana", "בננה"], ["Strawberry", "תות"]])
+  },
+  {
+    name: "Family", id: 2, last_change_date: new Date("1/8/2023"),
+    origin_lang: LanguageEnum.English, target_lang: LanguageEnum.Hebrew,
+    zugot: new Map<string, string>([["Father", "אבא"], ["Mother", "אמא"], ["Family", "משפחה"]])
+  },
+  {
+    name: "Animals", id: 3, last_change_date: new Date("1/5/2022"),
+    origin_lang: LanguageEnum.English, target_lang: LanguageEnum.Hebrew,
+    zugot: new Map<string, string>([["Dog", "כלב"], ["Elephant", "פיל"], ["Cat", "חתול"]])
+  }
+];
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
