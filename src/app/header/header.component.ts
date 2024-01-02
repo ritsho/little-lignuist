3f864b7fa56f2889520957898f2e9ffd4af6ca7a
Rivky Shoham <ritsho@gmail.com>
2024-01-02 20:26:37 +0200

    added toolbar to the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
