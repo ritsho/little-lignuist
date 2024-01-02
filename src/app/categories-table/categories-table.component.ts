903e878d6d4cf192fa2ad4ffb9413ceccaa7d721
Rivky Shoham <ritsho@gmail.com>
2024-01-03 00:13:00 +0200

    categories-table design

--- src/app/categories-table/categories-table.component.ts
+++ src/app/categories-table/categories-table.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { LIST_OF_CATEGORIES } from '../../main';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-categories-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CommonModule, MatIconModule],
   templateUrl: './categories-table.component.html',
   styleUrl: './categories-table.component.css'
 })
 export class CategoriesTableComponent {
-
+  displayedColumns: string[] = ['name', 'zugot', 'last_change_date', 'actions'];
+  dataSource = LIST_OF_CATEGORIES;
+ 
 }
