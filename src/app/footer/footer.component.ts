b9a74913d002bb9d00184df51b312f6069ce9e92
Rivky Shoham <ritsho@gmail.com>
2024-01-06 22:55:05 +0200

    add footer help icon

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatToolbarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
