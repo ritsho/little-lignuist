cf9fdbc7fb7a2e132118c64b8ece6570d1a39aae
Rivky Shoham <ritsho@gmail.com>
2024-01-07 18:17:58 +0200

    add new test item

--- src/app/categories-table/categories-table.component.ts
+++ src/app/categories-table/categories-table.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { LIST_OF_CATEGORIES, WordsCategory } from '../../main';
+import { LanguageEnum, WordsCategory } from '../../main';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-categories-table',
@@ -15,10 +14,22 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class CategoriesTableComponent {
   displayedColumns: string[] = ['name', 'zugot', 'last_change_date', 'actions'];
-  my_data = LIST_OF_CATEGORIES;
+  myData: WordsCategory[] = [
+    new WordsCategory("Food", 1, new Date("1/1/2024"),
+      LanguageEnum.English, LanguageEnum.Hebrew,
+      new Map<string, string>([["Apple", "תפוח"], ["Orange", "תפוז"], ["Banana", "בננה"], ["Strawberry", "תות"]])),
+  
+    new WordsCategory("Family", 2, new Date("1/8/2023"),
+      LanguageEnum.English, LanguageEnum.Hebrew,
+      new Map<string, string>([["Father", "אבא"], ["Mother", "אמא"], ["Family", "משפחה"]])),
+  
+    new WordsCategory("Animals", 3, new Date("1/5/2022"),
+      LanguageEnum.English, LanguageEnum.Hebrew,
+      new Map<string, string>([["Dog", "כלב"], ["Elephant", "פיל"], ["Cat", "חתול"]]))
+  ];
 
   deleteItem(itemToRemove: WordsCategory) {
-    this.my_data = this.my_data.filter(item => item !== itemToRemove);
+    this.myData = this.myData.filter(item => item !== itemToRemove);
   }
 
   editItem() {
@@ -26,6 +37,13 @@ export class CategoriesTableComponent {
   }
 
   newCategory() {
-    console.log("add new item - do nothing for now.");
+    console.log("adding test item");
+    let newItem = new WordsCategory("test", this.myData.length + 1,
+      new Date(), LanguageEnum.English, LanguageEnum.Hebrew,
+      new Map<string, string>([["Test", "בדיקה"]]));
+
+    let newList = Array.from(this.myData);
+    newList.push(newItem);
+    this.myData = newList;
   }
 }
