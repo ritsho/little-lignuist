2a1a578cdec0f9081b78e52580ab04f4a6cf4780
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2024-01-30 22:43:44 +0200

    add category validation and some UI changes

--- src/app/edit-category/edit-category.component.ts
+++ src/app/edit-category/edit-category.component.ts
@@ -11,13 +11,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ManageCategoriesService } from '../manage-categories.service';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-edit-category',
   standalone: true,
   templateUrl: './edit-category.component.html',
   styleUrl: './edit-category.component.css',
-  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, NgFor, MatIconModule, MatFormFieldModule, MatInputModule]
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, NgFor, MatIconModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule]
 })
 export class EditCategoryComponent {
 
@@ -48,15 +51,26 @@ export class EditCategoryComponent {
   save() {
     // TODO: add validations:
     // 1. check that this category name is NOT already exist
-    // 2. (Bonus) check that there's at least ONE TranslatedWord before saving
+    if (this.categoryToEdit.name == "") {
+      alert("Error - category must have a name");
+      return;
+    }
+
+    // check that there's at least ONE TranslatedWord before saving
+    if (this.categoryToEdit.words.length == 0 ||
+      this.categoryToEdit.words.find(item => item.origin.trim() === "") ||
+      this.categoryToEdit.words.find(item => item.target.trim() === "")) {
+      alert("Error - must have at least one valid pair, and all pairs should be filled");
+      return;
+    }
 
     // if this category is NEW
-    if (this.categoryToEdit.id == 0){
+    if (this.categoryToEdit.id == 0) {
 
       // update the ID, and save
       this.mc.add(this.categoryToEdit);
     }
-    
+
     // go to home page
     this.router.navigate(['']);
   }
