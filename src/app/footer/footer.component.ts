d440806774fa83a0148645e25eedc2dcff071426
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2024-08-06 20:15:52 +0300

    fix footer icon

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule, MatToolbarModule, RouterLink],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
