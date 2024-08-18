672162a96424b877dd81d8c77cba1540d548883f
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2024-08-18 21:36:35 +0300

    add exit-button and exit-dialog

---
+++ src/app/exit-button/exit-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exit-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './exit-button.component.html',
+  styleUrl: './exit-button.component.css'
+})
+export class ExitButtonComponent {
+
+}
---
+++ src/app/exit-dialog/exit-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exit-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './exit-dialog.component.html',
+  styleUrl: './exit-dialog.component.css'
+})
+export class ExitDialogComponent {
+
+}
