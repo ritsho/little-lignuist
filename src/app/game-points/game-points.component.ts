4280b82a22dbfb4ff2f0825377eb85a6e81f5a59
Oshrat <oshratrata12@gmail.com>
2024-08-18 21:20:52 +0300

    new component game points

---
+++ src/app/game-points/game-points.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-points',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-points.component.html',
+  styleUrl: './game-points.component.css'
+})
+export class GamePointsComponent {
+
+}
