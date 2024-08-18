2f738a9a2aabbddd94e49219d387c5619fe0ee42
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2024-08-18 21:48:32 +0300

    add game-over dialog

---
+++ src/app/game-over-dialog/game-over-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-over-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-over-dialog.component.html',
+  styleUrl: './game-over-dialog.component.css'
+})
+export class GameOverDialogComponent {
+
+}
