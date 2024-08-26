bd0ce02344b1a8035ceb5448072871472f41de63
Oshrat <oshratrata12@gmail.com>
2024-08-26 20:49:43 +0300

    los

--- src/app/exit-dialog/exit-dialog.component.ts
+++ src/app/exit-dialog/exit-dialog.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogClose } from '@angular/material/dialog';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-exit-dialog',
   standalone: true,
   imports: [MatDialogClose, MatButtonModule],
   templateUrl: './exit-dialog.component.html',
-  styleUrl: './exit-dialog.component.css'
+  styleUrl: './exit-dialog.component.css',
 })
 export class ExitDialogComponent {
+  constructor(private router: Router) {
+  }
 
+  navigateToMain() {
+    this.router.navigate(['choose-game']);
+  }
 }
